cf437f0d9584bc7d6d9e31828e18a401d89847f7
unknown <anunobijustice686@gmail.com>
2023-06-21 01:15:24 +0100

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,38 @@
+export const data = [
+    {
+      id: 1,
+      name: 'Video File',
+      type: 'video',
+      path: "/file-server/VID.mp4"
+    },
+    {
+      id: 2,
+      name: 'Audio File',
+      type: 'audio',
+      path: "/file-server/Maroon-5-Memories.mp3"
+    },
+    {
+      id: 3,
+      name: 'Document File',
+      type: 'document',
+      path: "/file-server/Test2.pdf"
+    },
+    {
+      id: 4,
+      name: 'Image File',
+      type: 'image',
+      path: "/file-server/download (1).jpeg"
+    },
+    {
+        id: 5,
+        name: 'Image File',
+        type: 'image',
+        path: "/file-server/images (4).jpeg"
+      },
+      {
+        id: 6,
+        name: 'Image File',
+        type: 'image',
+        path: "/file-server/images (6).jpeg"
+      },
+  ]
